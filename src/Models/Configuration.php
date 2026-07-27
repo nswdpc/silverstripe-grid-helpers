02d84d239736dd998490a004d739ae87fa1988bc
JamesDPC <69664712+JamesDPC@users.noreply.github.com>
2026-07-27 23:08:33 +0000

    [rector] Automated updates generated by rector configuration

--- src/Models/Configuration.php
+++ src/Models/Configuration.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NSWDPC\GridHelper\Models;
 
 use SilverStripe\Core\Extensible;
--- tests/ColumnCountTest.php
+++ tests/ColumnCountTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NSWDPC\GridHelper\Tests;
 
 use NSWDPC\GridHelper\Extensions\ElementChildGridExtension;
